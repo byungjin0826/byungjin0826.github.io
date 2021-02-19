62176b9e085211a33f110f799981f0b23d5ee54b
byungjin0826 <byungjin0826@gmail.com>
2021-02-19 10:31:39 +0900

    test

--- assets/search.js
+++ assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/2018/07/01/welcome.html"},{'title':"Welcome",'url':"/2021/02/19/test.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[]};
