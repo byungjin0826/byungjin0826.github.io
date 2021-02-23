7b787ad17e24e2a25dc5c171c2a104ecf0cc8fe0
byungjin0826 <byungjin0826@gmail.com>
2021-02-23 18:15:13 +0900

    update 2020-2-23

--- assets/search.js
+++ assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"블로그를 시작하며",'url':"/2021/02/19/about_create_blog.html"},{'title':"python 병렬처리와 ray에 대하여",'url':"/2021/02/19/python_multiprocessing.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"블로그를 시작하며",'url':"/2021/02/19/about_create_blog.html"},{'title':"python 병렬처리와 ray에 대하여",'url':"/2021/02/23/python_multiprocessing.html"}]};
