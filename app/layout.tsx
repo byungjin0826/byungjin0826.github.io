c000a4ae93aa913d95808547a41cc663765d4ba9
byungjinshin <byungjin0826@gmail.com>
2024-03-18 01:05:23 +0900

    구글 서치

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,6 +64,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       className={`${space_grotesk.variable} scroll-smooth`}
       suppressHydrationWarning
     >
+      <meta name="google-site-verification" content="zE1hn826qsD4PvPhHw5FwPU5DglJENtomClMgLQVFek" />
       <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
