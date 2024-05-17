7bb00da625ce8f213e5279edd9fbc04f0e2dca1f
byungjinshin <byungjin0826@gmail.com>
2024-05-17 09:16:35 +0900

    google analytics 안되는 문제 수정 중

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -26,7 +26,7 @@ const siteMetadata = {
     // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
     // umamiAnalytics: {
     //   // We use an env variable for this site to avoid other users cloning our analytics ID
-    umamiWebsiteId: '91cdb13d-e596-4c55-87ca-e3232d5f0c08', // e.g. 123e4567-e89b-12d3-a456-426614174000
+    // umamiWebsiteId: '91cdb13d-e596-4c55-87ca-e3232d5f0c08', // e.g. 123e4567-e89b-12d3-a456-426614174000
     //   // You may also need to overwrite the script if you're storing data in the US - ex:
     // src: 'https://us.umami.is/script.js',
     //   // Remember to add 'us.umami.is' in `next.config.js` as a permitted domain for the CSP
