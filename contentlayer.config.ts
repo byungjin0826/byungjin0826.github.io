6567b91dd59895b31230785a98aab05feb3f2c78
byungjinshin <byungjin0826@gmail.com>
2024-08-19 01:33:35 +0900

    원래대로

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -77,8 +77,8 @@ function createSearchIndex(allBlogs) {
 
 export const Blog = defineDocumentType(() => ({
   name: 'Blog',
-  filePathPattern: 'blog/**/*.md',
-  contentType: 'markdown',
+  filePathPattern: 'blog/**/*.mdx',
+  contentType: 'mdx',
   fields: {
     title: { type: 'string', required: true },
     date: { type: 'date', required: true },
