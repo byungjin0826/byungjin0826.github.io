fb49a6e3dfbdb1783af4591dd0cc0af7bfda3cd0
byungjinshin <byungjin0826@gmail.com>
2024-08-19 01:29:40 +0900

    ㅇㅇ

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -110,41 +110,6 @@ export const Blog = defineDocumentType(() => ({
   },
 }))
 
-export const BlogMD = defineDocumentType(() => ({
-  name: 'Blog',
-  filePathPattern: 'blog/**/*.md',
-  contentType: 'markdown',
-  fields: {
-    title: { type: 'string', required: true },
-    date: { type: 'date', required: true },
-    tags: { type: 'list', of: { type: 'string' }, default: [] },
-    lastmod: { type: 'date' },
-    draft: { type: 'boolean' },
-    summary: { type: 'string' },
-    images: { type: 'json' },
-    authors: { type: 'list', of: { type: 'string' } },
-    layout: { type: 'string' },
-    bibliography: { type: 'string' },
-    canonicalUrl: { type: 'string' },
-  },
-  computedFields: {
-    ...computedFields,
-    structuredData: {
-      type: 'json',
-      resolve: (doc) => ({
-        '@context': 'https://schema.org',
-        '@type': 'BlogPosting',
-        headline: doc.title,
-        datePublished: doc.date,
-        dateModified: doc.lastmod || doc.date,
-        description: doc.summary,
-        image: doc.images ? doc.images[0] : siteMetadata.socialBanner,
-        url: `${siteMetadata.siteUrl}/${doc._raw.flattenedPath}`,
-      }),
-    },
-  },
-}))
-
 export const Authors = defineDocumentType(() => ({
   name: 'Authors',
   filePathPattern: 'authors/**/*.mdx',
